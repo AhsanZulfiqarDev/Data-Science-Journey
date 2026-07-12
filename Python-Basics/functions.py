0dc8fda4c6b49bc1e3edf4b59943b0f381040cd0
Ahsan Zulfiqar <ahsanzulfiqar318@gmail.com>
2026-07-12 16:25:14 -0700

    Continue Python fundamentals practice

--- Python-Basics/functions.py
+++ Python-Basics/functions.py
@@ -51,3 +51,158 @@ def sortList(L):
 
 sortedList = sortList([10, 20, 30, 5, 40])
 print(sortedList)
+
+#"""LAMBDA FUNCTIONS"""
+square = lambda number:number**2
+print(square(5))
+
+
+add =lambda a,b:a+b
+print(add(10,20))
+
+
+students=[
+    ("Ali",73),
+    ("ahsan",13),
+    ("Ahmad",63),  
+]
+
+students.sort(key=lambda student:student[1])
+print(students)
+
+
+
+#traditional loop
+numbers =[1,2,3,4,5]
+
+square =[]
+for number in numbers:
+    square.append(number**2)
+    
+print(square)    
+
+#maps
+def square(number):
+    return number**2
+squares =map(square,numbers)
+print(squares)
+
+temperatures_celsius =[20,25,30,35]
+temperatures_fahrenheit =list(
+    map(
+        lambda temperature:(temperature*9/5)+32,
+        temperatures_celsius,
+    )
+)
+
+print(temperatures_fahrenheit)
+
+
+
+
+#filter() keeps elements for which a function returns true
+
+numbers =[1,2,3,4,5,6]
+#we only want even numbers
+
+even_numbers=[]
+for number in numbers:
+    if number%2==0:
+        even_numbers.append(number)
+     
+#using filter
+
+def is_even(number):
+    return number%2==0
+even_numbers =list(filter(is_even,numbers))
+
+print(even_numbers)
+
+#data oriented example
+sales=[1200,500,400,3400,800,2200]
+high_sales =list(
+    filter(lambda sale:sale>=2000,sales)
+    
+)
+
+print(high_sales)
+
+
+
+
+#zip( combines multiple iterables element by elemnt.)
+
+names =["Ali","Sara","Ahmad"]
+scores =[75,92,68]
+
+combined =zip(names,scores)
+print(list(combined))
+
+
+names =["Ali","Sara","Ahmad"]
+scores =[45,32,32]
+for name,score in zip(names,scores):
+      print(f"{name}: {score}")
+          
+#creating dictionary
+student_scores=dict(zip(names,scores))
+print(student_scores)
+#enumerate()
+# frequently be needing item and its position
+
+students =["Ali","Sara","Ahmad"]
+for i in range(len(students)):
+    print(i,students[i])
+
+for index,student in enumerate(students):
+    print(index,student)
+    
+ ##list comprehensions
+ 
+numbers = [1,2,3,4,5]
+squares =[]
+for number in numbers:
+    squares.append(numbers**2)
+
+square =[number**2 for number in numbers]
+
+numbers =[1,2,3,4,5,6]
+even_numbers = [
+    number
+    for number in numbersif number %2==0
+]            
+
+# transofrmation +filtering
+
+squared_even_numbers =[
+    number**2
+    for number in numbers
+    if number%2==0
+]
+
+#Iterable vs iterators
+#A list is an iterable:
+
+numbers =[10,20,30]
+
+for number in numbers:
+    print(number)
+    
+#other iterable list tuple string dictionary file range    
+
+
+#generators
+
+def get_number():
+    return[1,2,3]
+
+def generate_numbers():
+    yield 1
+    yield 2
+    yield 3
+    
+numbers =generate_numbers()
+print(numbers)
+    
+    
+
